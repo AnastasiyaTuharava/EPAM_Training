56727fc175fe75f7a626d897c128f091162406d5
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-16 21:33:45 +0300

    Add files via upload

---
+++ Lab01/pkg0103/Main.java
@@ -0,0 +1,17 @@
+
+package pkg0103;
+
+/*
+Algorithm reference
+https://drive.google.com/file/d/1R_03PDnl3ZeQbi8FNTAC3ugYHkwoaRfr/view?usp=sharing
+*/
+
+public class Main {
+
+    public static void main(String[] args) {
+        double r1 = 12.7, r2 = 9.5;
+        
+        System.out.println("Ring Area is " + RingArea.RingArea(r1, r2));
+    }
+    
+}
---
+++ Lab01/pkg0103/RingArea.java
@@ -0,0 +1,14 @@
+
+package pkg0103;
+
+public class RingArea {
+    
+    public static double RingArea (double r1, double r2) {
+        double pi = 3.14;
+        
+        double s = pi * (r1 * r1 - r2 * r2);
+        
+        return s;
+    }
+    
+}
