5e154342cc28d14a30df3561ccabba9ddc41139e
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-17 07:16:44 +0300

    Add files via upload

---
+++ Lab01/pkg0107/Change.java
@@ -0,0 +1,15 @@
+
+package pkg0107;
+
+public class Change {
+    
+    public static void Change (int a, int b) {
+        
+        b += a;
+        a = b - a;
+	b -= a;	 
+		
+	System.out.println("Now a is " + a);
+	System.out.println("Now b is "+ b);       
+    }   
+}
---
+++ Lab01/pkg0107/Main.java
@@ -0,0 +1,12 @@
+
+package pkg0107;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10, b = 6;
+        
+        Change.Change(a, b);
+    }
+    
+}
