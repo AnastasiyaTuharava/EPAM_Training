4d5ce9f5a8b0044d7527cdd134ded73fa614542e
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-20 22:43:31 +0300

    Updated

--- Lab01/pkg0101/Main.java
+++ Lab01/pkg0101/Main.java
@@ -1,16 +1,17 @@
 
 package pkg0101;
 
-public class Main {
+public class Task0101 {
+
     public static void main(String[] args) {
-        double dinoMassKg = 15700.05;
+        double dinoMassKg = UserInput.getDinoMassKG("Enter Dino Mass (kg), please:");
         
         double dinoMassGr = MassConversion.ConvertToGrams(dinoMassKg);
         double dinoMassMg = MassConversion.ConvertToMilligrams(dinoMassKg);
         double dinoMassTn = MassConversion.ConvertToTons(dinoMassKg);
-                
-        System.out.format("Dino mass in grams is %.1f%n", dinoMassGr);
-        System.out.format("Dino mass in milligrams is %.1f%n", dinoMassMg);  
-        System.out.format("Dino mass in tons is %.6f%n", dinoMassTn);            
-    } 
+        
+        Output.print("Dino mass in gr: ", dinoMassGr);
+        Output.print("Dino mass in mg: ", dinoMassMg);
+        Output.print("Dino mass in tn: ", dinoMassTn);
+    }
 }
