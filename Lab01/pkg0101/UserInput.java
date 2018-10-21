e49432240dc6fd3c772152e3dfac0597f7a01be0
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 09:16:37 +0300

    Updated

--- Lab01/pkg0101/UserInput.java
+++ Lab01/pkg0101/UserInput.java
@@ -7,8 +7,6 @@ public class UserInput {
     public static double getDinoMassKG (String str) {
         System.out.println(str);
         Scanner in = new Scanner(System.in);
-        double temp = in.nextDouble();
-        return temp;
-    }
-    
+        return in.nextDouble();
+    } 
 }
