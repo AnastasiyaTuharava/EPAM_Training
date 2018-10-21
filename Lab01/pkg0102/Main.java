e1fb277c94ac30a34f5c6f69da6c1bc9bbfc33e5
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 09:34:32 +0300

    Updated

--- Lab01/pkg0102/Main.java
+++ Lab01/pkg0102/Main.java
@@ -1,16 +1,18 @@
 
 package pkg0102;
 
-/* Algorithm reference
-https://drive.google.com/file/d/1bfkgzfXqLQrNsTKOi6g3nPnA7Q4EnIxU/view?usp=sharing
-*/
-
 public class Main {
 
     public static void main(String[] args) {
-        double a = 10.5, b = 3.2, c = 10.5;
+        double a = Input.getDouble("Enter a: ");
+        double b = Input.getDouble("Enter b: ");
+        double c = Input.getDouble("Enter c: ");
         
-        System.out.println("Three digits are the same is " + Check.TheSame(a,b,c));
-        System.out.println("Three numbers are different is " + Check.Different(a,b,c));
+        boolean equalityCheck = Check.checkEquality(a,b,c);
+        boolean inequalityCheck = Check.checkinequality(a,b,c);
+        
+        Output.print("Three digits are the same is ", equalityCheck);
+        Output.print("Three numbers are different is ", inequalityCheck);
     }
 }
+
