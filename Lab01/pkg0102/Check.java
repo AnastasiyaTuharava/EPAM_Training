73a6c102c5eed0654105a1ef28f222fcc2a39c14
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 09:35:10 +0300

    Updated

--- Lab01/pkg0102/Check.java
+++ Lab01/pkg0102/Check.java
@@ -2,10 +2,10 @@
 package pkg0102;
 
 public class Check {
-    public static boolean TheSame (double a, double b, double c) {
+    public static boolean checkEquality (double a, double b, double c) {
         return (a == b) && (b == c);
     }
-    public static boolean Different (double a, double b, double c) {
+    public static boolean checkinequality (double a, double b, double c) {
         return (a != b) && (b != c) && (c != a);
     }
 }
