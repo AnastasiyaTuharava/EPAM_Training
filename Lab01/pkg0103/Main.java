c306e972678741485365bce7f59e78a28823c584
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 11:26:15 +0300

    Updated

--- Lab01/pkg0103/Main.java
+++ Lab01/pkg0103/Main.java
@@ -1,17 +1,15 @@
 
 package pkg0103;
 
-/*
-Algorithm reference
-https://drive.google.com/file/d/1R_03PDnl3ZeQbi8FNTAC3ugYHkwoaRfr/view?usp=sharing
-*/
-
 public class Main {
 
     public static void main(String[] args) {
-        double r1 = 12.7, r2 = 9.5;
+        double r1 = Input.getDouble("Enter r1: ");
+        double r2 = Input.getDouble("Enter r2: ");
         
-        System.out.println("Ring Area is " + RingArea.RingArea(r1, r2));
-    }
-    
+        double ringArea = RingArea.calcRingArea(r1, r2);
+        
+        Output.print("Ring Area is ", ringArea);
+        
+    } 
 }
