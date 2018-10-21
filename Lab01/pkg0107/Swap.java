2a5cffa6a24c7e0c87e1a6ac1ab00886858dc2ec
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 12:22:02 +0300

    Updated

---
+++ Lab01/pkg0107/Swap.java
@@ -0,0 +1,15 @@
+
+package pkg0107;
+
+public class Swap {
+    
+       
+    public static void swapValues(int a, int b) {
+        a += b;
+        b = a-b;
+        a = a-b; 
+        
+        Output.print("Now a is ", a);
+        Output.print("Now b is ", b);   
+    }   
+}
