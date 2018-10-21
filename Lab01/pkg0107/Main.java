ed1ee7243c9291f7f2e2a15b5220bf1b3aa31936
AnastasiyaTuharava <44005417+AnastasiyaTuharava@users.noreply.github.com>
2018-10-21 12:22:38 +0300

    Updated

--- Lab01/pkg0107/Main.java
+++ Lab01/pkg0107/Main.java
@@ -1,12 +1,11 @@
-
 package pkg0107;
 
 public class Main {
 
     public static void main(String[] args) {
-        int a = 10, b = 6;
+        int a = Input.getInt("Enter integer a: ");
+        int b = Input.getInt("Enter integer b: ");
         
-        Change.Change(a, b);
+        Swap.swapValues(a, b);
     }
-    
 }
